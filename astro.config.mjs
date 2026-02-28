f0a218888ddec46f235b19bd00e8ac42e2bbdb28
根本壮馬 <nemotosoma@MacBookPro.AirPort>
2026-03-01 00:30:10 +0900

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nemotosoma.github.io',
+  site: 'https://nemo-works.github.io',
 });
