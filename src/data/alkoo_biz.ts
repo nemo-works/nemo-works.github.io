074f267ee469ccb1ecd59006f3569b4b6fa487a0
Soma Nemoto <soma.0820@gmail.com>
2026-03-01 22:04:34 +0900

    イベントグラフィックページ追加

--- src/data/alkoo_biz.ts
+++ src/data/alkoo_biz.ts
@@ -20,17 +20,17 @@ export const alkooBizWorks: AlkooBizWorkItem[] = [
   {
     id: 'event-graphics',
     listTitle: 'イベントグラフィックデザイン',
-    listDescription: '簡素なイベントでは、楽しいものでも楽しさが伝わらない。\nイベントパッケージの一環としてグラフィックデザインも行い、イベントの顔となって主催者を支援しています。',
-    thumbnail: { src: '/img/alkoo-biz/img_contents_alkoobiz_chura.jpg', alt: 'ちゅらウォーク' },
-    href: '/detail/alkoo/walk-route',
-    tags: ['グラフィックデザイン','販促物作成'],
+    listDescription: '顧客が検討したイベントの楽しさが伝わるように。\nパッケージの一環としてグラフィックデザインも行い、イベントの顔となって顧客を支援しています。',
+    thumbnail: { src: '/img/alkoo-biz/img_contents_alkoobiz_chura.jpg', alt: 'イベントグラフィックス' },
+    href: '/detail/biz/event-graphics',
+    tags: ['グラフィックデザイン','販促物作成','顧客折衝'],
   },
   {
     id: 'biz-page',
     listTitle: '法人商材紹介サイト',
     listDescription: '売上の大黒柱である、法人商材紹介サイトの作成・運用を行いました。',
     thumbnail: { src: '/img/alkoo-biz/img_contents_alkoobiz_pages.png', alt: '法人商材紹介ページ' },
-    href: null,
+    href: '/detail/biz/biz-page',
     tags: ['Web','開発','グロース施策'],
   },
 ];
