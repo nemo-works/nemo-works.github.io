2655c8f3a2b7013fe6f86ee21858a9b65f223971
Soma Nemoto <soma.0820@gmail.com>
2026-03-07 21:53:30 +0900

    コバトン事例追加・担当表記の追加

--- src/data/alkoo_biz.ts
+++ src/data/alkoo_biz.ts
@@ -9,28 +9,27 @@ export interface AlkooBizWorkItem {
 
 export const alkooBizWorks: AlkooBizWorkItem[] = [
   {
-    id: 'kobaton',
-    listTitle: '埼玉県様 コバトンALKOOマイレージ',
-    listDescription: 'サービス初となる自治体公式サービス。\n公式サービスの機能更新が自治体サービスへも還元。Win-Winな開発・運用で自治体の健康増進をサポート。',
-    thumbnail: { src: '/img/kobaton/kv.png', alt: 'コバトンALKOOマイレージ' },
-    href: '/detail/biz/kobaton',
-    tags: ['UIUX','機能検討','訴求物作成','顧客折衝'],
-  },
-  // ↓ 自治体イベントとしてまとめる？ちゅらとかかみがはら、やったことチラシ作成くらいじゃない？それなら川崎も入れられるよ
-  {
-    id: 'event-graphics',
-    listTitle: 'イベントグラフィックデザイン',
-    listDescription: '顧客が検討したイベントの楽しさが伝わるように。\nパッケージの一環としてグラフィックデザインも行い、イベントの顔となって顧客を支援しています。',
-    thumbnail: { src: '/img/alkoo-biz/img_contents_alkoobiz_chura.jpg', alt: 'イベントグラフィックス' },
-    href: '/detail/biz/event-graphics',
-    tags: ['グラフィックデザイン','販促物作成','顧客折衝'],
+    id: "event-graphics",
+    listTitle: "イベントグラフィックデザイン",
+    listDescription:
+      "顧客が検討したイベントの楽しさが伝わるように。\nパッケージの一環としてグラフィックデザインも行い、イベントの顔となって顧客を支援しています。",
+    thumbnail: {
+      src: "/img/alkoo-biz/img_contents_alkoobiz_chura.jpg",
+      alt: "イベントグラフィックス",
+    },
+    href: "/detail/biz/event-graphics",
+    tags: ["グラフィックデザイン", "販促物作成", "顧客折衝"],
   },
   {
-    id: 'biz-page',
-    listTitle: '法人商材紹介サイト',
-    listDescription: '売上の大黒柱である、法人商材紹介サイトの作成・運用を行いました。',
-    thumbnail: { src: '/img/alkoo-biz/img_contents_alkoobiz_pages.png', alt: '法人商材紹介ページ' },
-    href: '/detail/biz/biz-page',
-    tags: ['Web','開発','グロース施策'],
+    id: "biz-page",
+    listTitle: "法人商材紹介サイト",
+    listDescription:
+      "売上の大黒柱である、法人商材紹介サイトの作成・運用を行いました。",
+    thumbnail: {
+      src: "/img/alkoo-biz/img_contents_alkoobiz_pages.png",
+      alt: "法人商材紹介ページ",
+    },
+    href: "/detail/biz/biz-page",
+    tags: ["Web", "開発", "グロース施策"],
   },
 ];
